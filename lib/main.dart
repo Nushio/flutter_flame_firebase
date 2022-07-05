a1a622684558ad828e3d66580ed92dd5ce4617e7
Juan Rodriguez <nushio@gmail.com>
2022-07-05 09:58:10 -0500

    firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final game = FlameGame();
   runApp(GameWidget(game: game));
 }
