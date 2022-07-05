3814fd50178164ddb43f44b9ec48c5fd92a960e3
Juan Rodriguez <nushio@gmail.com>
2022-07-05 10:28:50 -0500

    Add TutorialGame, MainMenu Widget Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,59 @@
 import 'package:flame/game.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_flame_firebase/game/tutorial_game.dart';
+import 'package:flutter_flame_firebase/widgets/main_menu.dart';
 import 'firebase_options.dart';
 
+/// This is the single instance of our TutorialGame which
+/// will be reused throughout the lifecycle of the game.
+TutorialGame _tutorialGame = TutorialGame();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  final game = FlameGame();
-  runApp(GameWidget(game: game));
+  runApp(const TutorialGameApp());
+}
+
+class TutorialGameApp extends StatelessWidget {
+  const TutorialGameApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tutorial Game',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        // Settings up some default theme for elevated buttons.
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            fixedSize: const Size(200, 60),
+          ),
+        ),
+      ),
+      home: Scaffold(
+        body: GameWidget(
+          // This will display a loading bar until TutorialGame completes
+          // its onLoad method.
+          loadingBuilder: (conetxt) => const Center(
+            child: SizedBox(
+              width: 200,
+              child: LinearProgressIndicator(),
+            ),
+          ),
+          // Register all the overlays that will be used by this game.
+          overlayBuilderMap: {
+            MainMenu.id: (_, TutorialGame gameRef) => MainMenu(gameRef),
+          },
+          // By default MainMenu overlay will be active.
+          initialActiveOverlays: const [MainMenu.id],
+          game: _tutorialGame,
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/widgets/main_menu.dart
@@ -0,0 +1,61 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_flame_firebase/game/tutorial_game.dart';
+
+// This represents the main menu overlay.
+class MainMenu extends StatelessWidget {
+  // An unique identified for this overlay.
+  static const id = 'MainMenu';
+
+  // Reference to parent game.
+  final TutorialGame gameRef;
+
+  const MainMenu(this.gameRef, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          color: Colors.black.withAlpha(100),
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+              child: Wrap(
+                direction: Axis.vertical,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                spacing: 10,
+                children: [
+                  const Text(
+                    'Tutorial Game',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      gameRef.overlays.remove(MainMenu.id);
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
